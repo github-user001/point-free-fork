d1da27cd1aa6db1f882853506f575c78147f5b73
🫐🥢 <blueberrychopsticks@pm.me>
2022-06-10 18:08:32 -0400

    Add Deepgram client file

--- Examples/SpeechRecognition/SpeechRecognition/SpeechClient/Deepgram.swift
+++ Examples/SpeechRecognition/SpeechRecognition/SpeechClient/Deepgram.swift
@@ -1,5 +1,9 @@
 //
-// Created by laptop on 6/10/22.
+//  Deepgram.swift
+//  SpeechRecognition
+//
+//  Created by laptop on 6/10/22.
+//  Copyright © 2022 Brandon Williams. All rights reserved.
 //
 
 import Foundation
